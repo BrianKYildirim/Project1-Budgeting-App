4dc8e4bcbb9a6419172a80056fed17a1eb254a77
BrianYildirim <yildirimkbrian@gmail.com>
2024-12-20 09:27:36 -0500

    Added roots for login and signup pages.

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login.html', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout', methods=['GET', 'POST'])
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/signup.html', methods=['GET', 'POST'])
+def sign_up():
+    return render_template("signup.html")
