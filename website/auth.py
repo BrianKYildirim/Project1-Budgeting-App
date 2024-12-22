9a78df235ff54cf8b5034c24c1ed8a675540ff9a
BrianYildirim <yildirimkbrian@gmail.com>
2024-12-22 13:30:14 -0500

    Added links to the dashboard, budgets, and insights webpages.

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -12,14 +13,23 @@ def login():
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
         if user:
-            pass
+            if check_password_hash(user.password, password):
+                flash('Login successful.', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.dashboard'))
+            else:
+                flash('Login failed (incorrect password).', category='error')
+        else:
+            flash('Login failed (email does not exist).', category='error')
     data = request.form
     print(data)
     return render_template("login.html")
 
-@auth.route('/logout', methods=['GET', 'POST'])
+@auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup.html', methods=['GET', 'POST'])
 def sign_up():
@@ -41,26 +51,29 @@ def sign_up():
             if not char.isalnum():
                 special = True
 
-        if not email or not firstname or not password1 or not password2:
-            flash('Please fill out all of the fields.', category='Error')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif not email or not firstname or not password1 or not password2:
+            flash('Please fill out all of the fields.', category='error')
         elif len(email) < 4:
-            flash("Email must be at least 4 characters.", category="Error")
+            flash("Email must be at least 4 characters.", category="error")
         elif len(firstname) < 2:
-            flash("First name must be at least 2 characters.", category="Error")
+            flash("First name must be at least 2 characters.", category="error")
         elif password1 != password2:
-            flash("Passwords do not match.", category="Error")
+            flash("Passwords do not match.", category="error")
         elif len(password1) < 8:
-            flash("Password must be at least 8 characters.", category="Error")
+            flash("Password must be at least 8 characters.", category="error")
         elif not num or not uppercase or not special:
             flash(
                 "Password must include at least one uppercase letter, one number, and one symbol.",
-                category="Error")
+                category="error")
         else:
             new_user = User(email = email, firstname = firstname, password = generate_password_hash(password1, method='scrypt', salt_length=16))
             db.session.add(new_user)
             db.session.commit()
-            flash("Account created!", category="Success")
+            login_user(user, remember=True)
+            flash("Account created!", category="success")
             return redirect(url_for('views.dashboard'))
-
 
     return render_template("signup.html")
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
@@ -7,5 +8,14 @@ def index():
     return render_template("index.html")
 
 @views.route('/dashboard.html')
+@login_required
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", user=current_user)
+
+@views.route('/budgets.html')
+def budgets():
+    return render_template("budgets.html")
+
+@views.route('/insights.html')
+def insights():
+    return render_template("insights.html")
